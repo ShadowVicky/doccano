cacd31c2bd147311a732bde072f1a0f3cb867b8f
Shadow Vicky <88448717+ShadowVicky@users.noreply.github.com>
2025-08-03 12:10:22 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-
-# Initialize DB only on first run
-if [ ! -f /data/.init_done ]; then
-  doccano init
-  doccano createuser --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" --email "$ADMIN_EMAIL"
-  touch /data/.init_done
-fi
-
-# Start webserver
-doccano webserver --port 8000
+doccano init
+doccano createuser --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" --email "$ADMIN_EMAIL"
